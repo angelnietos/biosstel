2b623bb8a5142f82655482faeb2ad3e6da03e1d9
angel <angel.nieto@babbooni.com>
2026-02-26 03:16:28 +0100

    removed duplicated old files

--- libs/backend/api-fichajes/src/application/cqrs/handlers/index.ts
+++ libs/backend/api-fichajes/src/application/cqrs/handlers/index.ts
@@ -1,2 +1,4 @@
-export * from './fichajes';
-export * from './task';
+export * from './fichajes/commands';
+export * from './fichajes/queries';
+export * from './task/commands';
+export * from './task/queries';
