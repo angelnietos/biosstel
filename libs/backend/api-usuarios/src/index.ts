7605273ec336a495e87662d1fce05f43018450ad
angel <angel.nieto@babbooni.com>
2026-02-26 14:47:51 +0100

    fix

--- libs/backend/api-usuarios/src/index.ts
+++ libs/backend/api-usuarios/src/index.ts
@@ -1,10 +1,9 @@
 /**
  * @biosstel/api-usuarios - API Usuarios (CRUD, roles, clientes, documentos)
- * Arquitectura hexagonal: application (ports + use-cases) + infrastructure (api, graphql, postgres).
+ * Arquitectura hexagonal: application (ports + use-cases) + infrastructure (postgres, graphql, adapters).
  */
 export * from './domain';
 export * from './application';
 export * from './infrastructure';
 export * from './users.module';
 export * from './users.service';
-export * from './infrastructure/api';
