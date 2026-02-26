b94004c5e8b775f008f92456b637e2829d663ad3
angel <angel.nieto@babbooni.com>
2026-02-26 04:12:04 +0100

    fix old imports

--- libs/backend/api-alertas/src/application/cqrs/commands/alertas/CreateAlerta.command.ts
+++ libs/backend/api-alertas/src/application/cqrs/commands/alertas/CreateAlerta.command.ts
@@ -1,5 +1,5 @@
 import type { ICommand } from '@biosstel/api-shared';
-import type { CreateAlertData } from '../../../../../domain/repositories';
+import type { CreateAlertData } from '../../../../domain/repositories';
 
 export class CreateAlertaCommand implements ICommand {
   readonly type = 'CreateAlertaCommand';
