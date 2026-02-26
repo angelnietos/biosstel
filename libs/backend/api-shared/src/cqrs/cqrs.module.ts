85bd9d4d40a9859a460f37af52443cc4ec253551
angel <angel.nieto@babbooni.com>
2026-02-26 15:40:12 +0100

    forwardRef y un factory para inyectar correctamente el ModuleRef

--- libs/backend/api-shared/src/cqrs/cqrs.module.ts
+++ libs/backend/api-shared/src/cqrs/cqrs.module.ts
@@ -1,10 +1,15 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, Module, forwardRef } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
 import { Mediator } from './mediator';
 import { IMediator } from './markers';
 
 @Global()
 @Module({
   providers: [
+    {
+      provide: ModuleRef,
+      useExisting: forwardRef(() => ModuleRef),
+    },
     Mediator,
     {
       provide: IMediator,
